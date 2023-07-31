1ec4720abbead629145ca6c3f7506ba752a92eb0
Domingo Gonzalez <domingo_gonzalez@icloud.com>
2023-07-31 20:27:39 +0100

    new spring training

--- superheroes-ui/src/services/hero-service.js
+++
@@ -1,9 +0,0 @@
-import http from './axios-common';
-
-export function getAllHeroes(){
-    return http.get("/heroes");
-}
-
-export function saveHero(hero){
-    return http.post("/heroes", hero)
-}
